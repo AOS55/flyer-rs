28986f056f8c1b0262e4e8b15e710b4f202dcf71
AOS55 <aq15777@bristol.ac.uk>
2023-09-29 21:38:20 +0100

    makes a bigmap

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,10 @@ use world::World;
 fn main() {
 
     let mut w = World::default();
+    w.set_screen_dims(3840.0, 3840.0);
     w.create_map(1, None, None, Some(true));
 
     let pixmap = w.render();
-    pixmap.save_png("image.png").unwrap();
-
+    // println!("{:?}", pixmap.data());
+    pixmap.save_png("image_big.png").unwrap();
 }
