180a37ec15f2562bc8f06139d37c10535c9bedfc
AOS55 <aq15777@bristol.ac.uk>
2023-09-29 21:38:54 +0100

    added trim to library

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 mod terrain;
 mod aircraft;
 mod world;
+mod trim;
 
 pub use terrain::{Terrain, TerrainConfig, Tile, RandomFuncs, StaticObject};
 pub use aircraft::Aircraft;
 pub use world::{World, Camera, Settings};
+pub use trim::Trim;
