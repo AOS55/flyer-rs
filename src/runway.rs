faa44e3c8163d46017313fa2f5dafe512b625e30
AOS55 <aq15777@bristol.ac.uk>
2023-10-09 19:02:32 +0100

    add approach points

--- src/runway.rs
+++ src/runway.rs
@@ -1,4 +1,6 @@
 use glam::Vec2;
+use std::collections::HashMap;
+use std::f32::consts::PI;
 
 pub struct Runway {
     pub name: String,
@@ -39,6 +41,44 @@ impl Runway {
         ];
         
         is_point_inside_polygon(test_point, polygon)
+
+    }
+
+    pub fn approach_points(&self) -> HashMap<String, Vec<f32>> {
+        
+        let touchdown_fraction = 0.8;  // fraction of runway that is landable, used to determine touchdown point
+        let faf_dist = 3000.0;  // distance from final approach fix to touchdown_point [m]
+        let inaf_dist = 2000.0; // distance from intermediate fix to touchdown point [m]
+        let iaf_dist = 2000.0; // distance from initial approach fix to the intermediate approach fix
+        let intercept_angle = 30.0 * PI/180.0;  //
+
+        let faf_rot_vec = Vec2::from_angle(self.heading.to_radians() - PI);
+        let touchdown_point = faf_rot_vec.rotate(Vec2::new(self.dims[1] * (touchdown_fraction/2.0), 0.0)) + self.pos;
+
+        let faf = faf_rot_vec.rotate(Vec2::new(faf_dist, 0.0)) + touchdown_point;
+        
+        let inaf_l_rot_vec = Vec2::from_angle(self.heading.to_radians() - PI + intercept_angle);
+        let inaf_l = inaf_l_rot_vec.rotate(Vec2::new(inaf_dist, 0.0)) + faf;
+
+        let inaf_r_rot_vec = Vec2::from_angle(self.heading.to_radians() - PI - intercept_angle);
+        let inaf_r = inaf_r_rot_vec.rotate(Vec2::new(inaf_dist, 0.0)) + faf;
+
+        let iaf_l_rot_vec = Vec2::from_angle(self.heading.to_radians() - PI/2.0);
+        let iaf_l = iaf_l_rot_vec.rotate(Vec2::new(iaf_dist, 0.0)) + inaf_l;
+
+        let iaf_r_rot_vec = Vec2::from_angle(self.heading.to_radians() + PI/2.0);
+        let iaf_r = iaf_r_rot_vec.rotate(Vec2::new(iaf_dist, 0.0)) + inaf_r;
+
+        let points: HashMap<String, Vec<f32>> = HashMap::from([
+            ("touchdown".to_string(), vec![touchdown_point[0], touchdown_point[1]]),
+            ("faf".to_string(), vec![faf[0], faf[1]]),
+            ("inaf_l".to_string(), vec![inaf_l[0], inaf_l[1]]),
+            ("inaf_r".to_string(), vec![inaf_r[0], inaf_r[1]]),
+            ("iaf_l".to_string(), vec![iaf_l[0], iaf_l[1]]),
+            ("iaf_r".to_string(), vec![iaf_r[0], iaf_r[1]])
+        ]);
+
+        points
 
     }
 
