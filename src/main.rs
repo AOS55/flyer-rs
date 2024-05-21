88363e12e7d10f0b39960540428d26dc78c0c14d
AOS55 <aq15777@bristol.ac.uk>
2024-05-21 11:51:18 +0100

    test run of image on render

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,8 @@ fn main() {
     w.create_map(1, Some(vec![256, 256]), None, Some(true));
     w.create_runway();
     w.camera.move_camera(vec![0.0, 0.0, -1000.0]);
-    w.runway.unwrap().on_runway(Vec2::new(-499.0, -10.0));
-    // let pixmap = w.render();
+    // w.runway.unwrap().on_runway(Vec2::new(-499.0, -10.0));
+    let pixmap = w.render();
     // // println!("{:?}", pixmap.data());
-    // pixmap.save_png("image.png").unwrap();
+    pixmap.save_png("image-test.png").unwrap();
 }
